ecb01534adbad812475255913f8d16a7c7052fd4
whoysAndre <yellowcata2121@gmail.com>
2025-11-09 17:59:55 -0500

    Cors

--- src/main.ts
+++ src/main.ts
@@ -7,12 +7,18 @@ async function bootstrap() {
 
   app.setGlobalPrefix("api");
 
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    credentials: true,
+    methods: 'GET,POST,PUT,DELETE',
+  });
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
       forbidNonWhitelisted: true
     })
   )
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 4000);
 }
 bootstrap();
