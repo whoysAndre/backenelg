5e3ee669da90ad90e84e37e5c2fdd4f04c7c06ee
whoysAndre <yellowcata2121@gmail.com>
2025-11-19 08:32:45 -0500

    Reports Module fixed

--- src/reports/reports.module.ts
+++ src/reports/reports.module.ts
@@ -3,10 +3,11 @@ import { ReportsService } from './reports.service';
 import { ReportsController } from './reports.controller';
 import { CommonModule } from 'src/common/common.module';
 import { SalesModule } from 'src/sales/sales.module';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
   providers: [ReportsService],
   controllers: [ReportsController],
-  imports: [ CommonModule,SalesModule ]
+  imports: [ CommonModule,SalesModule, AuthModule ]
 })
 export class ReportsModule {}
